a3f2d517aa431700950e3a8b479442907057ad34
DisappointingToaster <mmotcats@gmail.com>
2021-07-01 00:29:06 +0300

    added middleware for security. created new table for posts for testing

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 class RegisterController extends Controller
 {
+    public function __construct()
+    {  
+       $this->middleware(['guest']); 
+    }
     public function index()
     {
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
 Route::get('dashboard','App\Http\Controllers\DashboardController@index')->name('dashboard');
+
+Route::post('logout','App\Http\Controllers\LogoutController@deauth')->name('logout');
 
 Route::get('login','App\Http\Controllers\LoginController@index')->name('login');
 Route::post('login','App\Http\Controllers\LoginController@authuser');
@@ -10,6 +16,6 @@ Route::post('login','App\Http\Controllers\LoginController@authuser');
 Route::get('register','App\Http\Controllers\RegisterController@index')->name('register');
 Route::post('register','App\Http\Controllers\RegisterController@store');
 
-Route::get('/', function () {
-    return view('posts.index');
-});
+Route::get('posts','App\Http\Controllers\PostController@index')->name('posts');
+Route::post('posts','App\Http\Controllers\PostController@store');
+
